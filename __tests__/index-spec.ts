890ca02ca1677b502f4d3975f7157e0ce1d0ffe6
Yu Chen <yu.chen@live.ie>
2018-04-30 17:20:53 +0800

    export resolveObject

--- __tests__/index-spec.ts
+++ __tests__/index-spec.ts
@@ -3,4 +3,5 @@ import * as index from '../src/index';
 test('Should have fetch available', () => {
   expect(index.fetch).toBeTruthy();
   expect(index.isPromise).toBeTruthy();
+  expect(index.resolveObject).toBeTruthy();
 });
