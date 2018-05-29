d4fb47588010055ab4b3cb2c44abd664afe89787
Yu Chen <yu.chen@live.ie>
2018-05-29 13:13:13 +0800

    add to readme

--- __tests__/loadScript-spec.ts
+++ __tests__/loadScript-spec.ts
@@ -3,8 +3,8 @@ import { loadScript } from '../src/loadScript';
 declare const window: any;
 
 test('Should load jssdk', async () => {
-  document.head.innerHTML = ``;
   try {
+    expect(window.wx).toBeFalsy();
     await loadScript('http://res.wx.qq.com/open/js/jweixin-1.2.0.js');
     expect(window.wx).toBeTruthy();
   } catch (e) {
