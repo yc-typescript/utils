aba0065240fd50982c707409bf9a67c3b2efaab3
Yu Chen <yu.chen@live.ie>
2018-05-29 13:10:21 +0800

    loadScript

--- __tests__/index-spec.ts
+++ __tests__/index-spec.ts
@@ -3,5 +3,6 @@ import * as index from '../src/index';
 test('Should have fetch available', () => {
   expect(index.fetch).toBeTruthy();
   expect(index.isPromise).toBeTruthy();
+  expect(index.loadScript).toBeTruthy();
   expect(index.resolveObject).toBeTruthy();
 });
